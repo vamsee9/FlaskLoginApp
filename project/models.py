dd837db4d9361369eb1ef88b7f6315b3c886c999
vamsee9 <4n0n5ur7@protonmail.com>
2021-04-10 10:08:35 +0530

    fixed server eror and name change dynamically
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- project/models.py
+++ project/models.py
@@ -1,7 +1,10 @@
+from flask_login import UserMixin
 from . import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys required for SQLAlchemy
-    email  =db.Column(db.String(100), unique=True)
+
+class User(UserMixin, db.Model):
+    # primary keys required for SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
