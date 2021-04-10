5942a670addf15ed09263b7f4427ef9c7ca199ca
vamsee9 <4n0n5ur7@protonmail.com>
2021-04-10 10:44:49 +0530

    fixed server overload and code improvements
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- project/__init__.py
+++ project/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-#init SQLAlchemy so we can use it later in our model
+# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'kYp3s6v9y$B?E(H+MbQeThWmZq4t7w!z' # for salting passwords
+    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4igorO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
--- project/models.py
+++ project/models.py
@@ -3,7 +3,7 @@ from . import db
 
 
 class User(UserMixin, db.Model):
-    # primary keys required for SQLAlchemy
+    # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
